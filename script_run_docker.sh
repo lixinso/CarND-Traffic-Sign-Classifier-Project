db0cd33d765fc3b9258bc0dd30ce6806047aa61c
Xinsong Li <lixinso@gmail.com>
2020-06-28 17:00:50 -0500

    local port 9999

--- script_run_docker.sh
+++ script_run_docker.sh
@@ -1,2 +1,2 @@
-docker run -it --rm --entrypoint "/run.sh" -p 8888:8888 -v `pwd`:/src udacity/carnd-term1-starter-kit
+docker run -it --rm --entrypoint "/run.sh" -p 9999:8888 -v `pwd`:/src udacity/carnd-term1-starter-kit
 
